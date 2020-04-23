c83b8910f9b1b5dd397d950a0f9dfdf83399a9e3
francolmenar <francolmenar@outlook.es>
2020-04-23 19:16:57 +0200

    Files cleaned and script renamed

--- lab2/compile_and_execute.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-python3 new_regex.py Problems/Problem1.java &&
-cd Problems &&
-javac -cp com.microsoft.z3.jar:. instProblem1.java &&
-java -cp com.microsoft.z3.jar:. instProblem1 Log true > /dev/null
-
--- lab2/execute.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-cd Problems &&
-javac -cp com.microsoft.z3.jar:. instProblem1.java &&
-java -cp com.microsoft.z3.jar:. instProblem1 > /dev/null
