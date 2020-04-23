43e2736a955752dce076e0aae0d103e5e81b7ba3
francolmenar <francolmenar@outlook.es>
2020-04-23 09:19:12 +0200

    Added -f functionality

--- lab1/test-regex.sh
+++ lab1/test-regex.sh
@@ -1,37 +1,96 @@
 #!/usr/bin/env bash
 
-pythonPath="./new_regex.py" # Path to the Python file
+pythonPath="./new_regex.py"      # Path to the Python file
 seqPath="../../RERS/sequential/" # First Path
-# Folders inside /sequential/
-# declare -a arrFolders=("TrainingSeqLtlRers2019/")
 
-declare -a arrFolders=("TrainingSeqLtlRers2019/" "SeqLtlRers2019/" "SeqReachabilityRers2019/" "TrainingSeqReachRers2019/")
+notWorking="../../RERS/sequential/TrainingSeqLtlRers2019/Problem1/Problem1.java"
+
+declare -a arrFolders=() # Array for the folders to be executed
+folder1="TrainingSeqLtlRers2019/"
+folder2="SeqLtlRers2019/"
+folder3="SeqReachabilityRers2019/"
+folder4="TrainingSeqReachRers2019/"
 
 fileType=".java" # File type
 
-for arrFolder_i in "${arrFolders[@]}"
-do
+############## Reading inputs ##############
+POSITIONAL=()
+while [[ $# -gt 0 ]]; do # Reading inputs
+  key="$1"
+
+  case $key in
+  -f | --folder) # Folder lab to be executed
+    IFS=',' read -ra FOLDER <<<"$2" # Separate by ","
+    shift                           # past argument
+    shift                           # past value
+    ;;
+  -h | --help)                           # TODO Add a help command
+    HELP="$2"
+    shift # past argument
+    shift # past value
+    ;;
+  *) # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift              # past argument
+    ;;
+  esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+if [[ -n $1 ]]; then
+  echo "Last line of file specified as non-opt/last argument:"
+  tail -1 "$1"
+fi
+
+############## Processing the parameters ##############
+if [ ${#FOLDER[@]} -ge 1 ]; then
+  for folder_i in "${FOLDER[@]}"; do
+    case $folder_i in
+    1 | ${folder1}) # Folder lab to be executed
+      arrFolders+=("${folder1}")
+      shift # past argument
+      shift # past value
+      ;;
+    2 | ${folder2}) # Folder lab to be executed
+      arrFolders+=("${folder2}")
+      shift # past argument
+      shift # past value
+      ;;
+    3 | ${folder3}) # Folder lab to be executed
+      arrFolders+=("${folder3}")
+      shift # past argument
+      shift # past value
+      ;;
+    4 |${folder4}) # Folder lab to be executed
+      arrFolders+=("${folder4}")
+      shift # past argument
+      shift # past value
+      ;;
+    esac
+  done
+else
+  arrFolders+=("${folder1}" "${folder2}" "${folder3}" "${folder4}")
+fi
+
+############## Runing the RERS programs ##############
+for arrFolder_i in "${arrFolders[@]}"; do
   # Construct the path
   newPath=${seqPath}${arrFolder_i}
-
   printf %"s\n\n" "${newPath}"
 
   # Check all the folders of the path
-  for file in "${newPath}"*;
-  do
-    IFS='/' read -ra clean <<< "$file" # Get the Problem name
-    problem=${clean[${#clean[@]} - 1]} # Get the name of the problem - Element after last "/"
+  for file in "${newPath}"*; do
+    IFS='/' read -ra clean <<<"$file"   # Get the Problem name
+    problem=${clean[${#clean[@]} - 1]}  # Get the name of the problem - Element after last "/"
     newFilePath="${newPath}${problem}/" # Path to the file's folder
 
     fileToRun=${problem}${fileType} # Next file to be executed
 
     # Check if the file exists
-    if [ -f "${newFilePath}${fileToRun}" ]; then
+    if [ -f "${newFilePath}${fileToRun}" ] && [ "${newFilePath}${fileToRun}" != $notWorking ]; then
       echo "${newFilePath}${fileToRun}"
       python "${pythonPath}" "${newFilePath}${fileToRun}" # Run the file
     fi
   done
+  printf "\n\n"
 done
-
-# TrainingSeqReachRers2019 lab1
-# SeqReachabilityRers2019 lab2, lab3
