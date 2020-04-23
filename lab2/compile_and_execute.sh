4517f1b3ed7785e703f92bf9a6125cd94808222e
francolmenar <francolmenar@outlook.es>
2020-04-23 19:09:28 +0200

    [lab2] Script added

--- lab2/compile_and_execute.sh
+++ lab2/compile_and_execute.sh
@@ -1,7 +1,7 @@
-#!/usr/bin/sh
+#!/usr/bin/env bash
 
 python3 new_regex.py Problems/Problem1.java &&
 cd Problems &&
 javac -cp com.microsoft.z3.jar:. instProblem1.java &&
-java -cp com.microsoft.z3.jar:. instProblem1 > /dev/null
+java -cp com.microsoft.z3.jar:. instProblem1 Log true > /dev/null
 
